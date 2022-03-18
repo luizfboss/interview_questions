8c24846fcec1aa51704d07473089a5d42a96d878
luizfboss <luiz.fbossetto@outlook.com>
2022-03-18 20:55:40 -0300

    Update narcissistic_number.py

--- narcissistic_number.py
+++ narcissistic_number.py
@@ -6,3 +6,27 @@
     #1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
 #The Challenge:
 #Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10. This may be #True and False in your language, e.g. PHP.
+
+def narcissistic(value):
+    arr = []
+    string = str(value)
+    size = len(string)
+
+    for n in string:
+      arr.append(n)
+
+    new_list = map(lambda x: int(x) ** size, arr)
+
+    pre_result = list(new_list)
+    result = sum(pre_result)
+
+    if result == value:
+      return f"{value} is narcissistic"
+    else:
+      return f"{value} is not narcissistic"
+
+
+print(narcissistic(7))
+print(narcissistic(371))
+print(narcissistic(122))
+print(narcissistic(4887))
