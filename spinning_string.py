a3f0e81dd6a49e23a754c8872f37c50d682cfa63
luizfboss <luiz.fbossetto@outlook.com>
2022-03-19 23:46:13 -0300

    Update spinning_string.py

--- spinning_string.py
+++ spinning_string.py
@@ -5,7 +5,20 @@
 # spinWords( "This is a test") => returns "This is a test" 
 # spinWords( "This is another test" )=> returns "This is rehtona test"
 
-
 def spin_words(sentence):
-    # Your code goes here
-    pass
+  list_string = sentence.split(' ')
+  new_list = []
+  for w in list_string:
+    if len(w) > 4:
+      new_list.append(w[::-1])
+    else:
+      new_list.append(w)
+  return ' '.join(new_list)
+
+# Single Words
+print(spin_words("Welcome")) # "emocleW"
+print(spin_words("to")) # "to"
+print(spin_words("CodeWars")) # "sraWedoC"
+# Multiple Words
+print(spin_words("Hey fellow warriors")) # "Hey wollef sroirraw"
+print(spin_words("This sentence is a sentence")) # "This ecnetnes is a ecnetnes"
