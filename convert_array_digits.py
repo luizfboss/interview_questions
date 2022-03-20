dffa4f015fe4d615e2fdb13b3c9c3b3217a62222
luizfboss <luiz.fbossetto@outlook.com>
2022-03-20 15:51:44 -0300

    Update convert_array_digits.py

--- convert_array_digits.py
+++ convert_array_digits.py
@@ -1,11 +1,11 @@
 # Convert number to reversed array of digits. Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
 
 def digitize(n):
-  return map(int, str(n)[::-1])
+  return [int(x) for x in str(n)[::-1]]
 
 
 print(digitize(0)) # [0]
-print(digitize()) # [1,3,2,5,3]
+print(digitize(35231)) # [1,3,2,5,3]
 print(digitize(23582357)) # [7,5,3,2,8,5,3,2]
 print(digitize(984764738)) # [8,3,7,4,6,7,4,8,9]
 print(digitize(45762893920)) # [0,2,9,3,9,8,2,6,7,5,4]
